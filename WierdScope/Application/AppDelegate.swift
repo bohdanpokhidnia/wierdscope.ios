b7d81820f973ef1ef10a04dcb6181536bf9e11b3
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2023-11-28 10:12:38 +0200

    removing ApplicationCheckProvider

--- WierdScope/Application/AppDelegate.swift
+++ WierdScope/Application/AppDelegate.swift
@@ -10,12 +10,19 @@ import GoogleMobileAds
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        let providerFactory = ApplicationCheckProvider()
-        AppCheck.setAppCheckProviderFactory(providerFactory)
-        
         FirebaseApp.configure()
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = ["d9bc6a519694f925838658a257fedacb"]
+        configureAds()
         return true
+    }
+}
+
+// MARK: - Private Methods
+
+private extension AppDelegate {
+    func configureAds() {
+        let mobileAds = GADMobileAds.sharedInstance()
+        
+        mobileAds.start()
+        mobileAds.requestConfiguration.testDeviceIdentifiers = ["d9bc6a519694f925838658a257fedacb"]
     }
 }
